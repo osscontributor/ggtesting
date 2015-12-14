aae7513fd7ca4667dcbd8c7baacbd08df0c941b2
Jeff Scott Brown <brownj@ociweb.com>
2015-12-14 12:19:56 +0000

    Add GebSpec

---
+++ grails-app/controllers/demo/PersonController.groovy
@@ -0,0 +1,107 @@
+package demo
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class PersonController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Person.list(params), model:[personCount: Person.count()]
+    }
+
+    def show(Person person) {
+        respond person
+    }
+
+    def create() {
+        respond new Person(params)
+    }
+
+    @Transactional
+    def save(Person person) {
+        if (person == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (person.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond person.errors, view:'create'
+            return
+        }
+
+        person.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'person.label', default: 'Person'), person.id])
+                redirect person
+            }
+            '*' { respond person, [status: CREATED] }
+        }
+    }
+
+    def edit(Person person) {
+        respond person
+    }
+
+    @Transactional
+    def update(Person person) {
+        if (person == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (person.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond person.errors, view:'edit'
+            return
+        }
+
+        person.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'person.label', default: 'Person'), person.id])
+                redirect person
+            }
+            '*'{ respond person, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Person person) {
+
+        if (person == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        person.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'person.label', default: 'Person'), person.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'person.label', default: 'Person'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
