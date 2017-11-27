2664617fec3a3e94457a6a107fba7f54c1328080
Jeff Scott Brown <brownj@objectcomputing.com>
2017-11-27 13:16:31 -0600

    Publish to gh-pages

--- travis-build.sh
+++ travis-build.sh
@@ -7,6 +7,33 @@ cd ../3.3.x
 
 EXIT_STATUS=0
 
+echo "Publishing Docs For Branch $TRAVIS_BRANCH"
+if [[ $TRAVIS_BRANCH =~ ^master$ && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+
+  cd ../
+  ./gradlew ascii || EXIT_STATUS=$?
+  echo "Docs status: $EXIT_STATUS"
+
+  git config --global user.name "$GIT_NAME"
+  git config --global user.email "$GIT_EMAIL"
+  git config --global credential.helper "store --file=~/.git-credentials"
+  echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+
+  git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
+  cd gh-pages
+
+  # If this is the master branch then update the snapshot
+  rm -rf *.html *.js images
+  cp -R ../build/asciidoc/html5/* .
+  git add .
+
+  git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID" && {
+    git push origin HEAD || true
+  }
+  cd ..
+  rm -rf gh-pages
+fi
+
 echo "Finished build with status: $EXIT_STATUS"
 
 exit $EXIT_STATUS
